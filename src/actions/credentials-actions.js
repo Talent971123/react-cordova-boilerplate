0b8e159064e7e2e6d50d5a76c61ad48d0d71fe97
Talent971123 <cheeyongchang1123@outlook.sg>
2023-10-31 02:42:45 -0700

    fix: credentials-action

--- src/actions/credentials-actions.js
+++ src/actions/credentials-actions.js
@@ -1,38 +1,46 @@
-import * as types from 'src/constants/credentials-action-types.js';
+import * as types from "src/constants/credentials-action-types.js";
 
 export function clearCredentials() {
   return {
-    type: types.CLEAR_CREDENTIALS
+    type: types.CLEAR_CREDENTIALS,
   };
 }
 export function checkCredentials() {
   return {
-    type: types.CHECK_CREDENTIALS
+    type: types.CHECK_CREDENTIALS,
   };
 }
 export function checkCredentialsSucess() {
   return {
-    type: types.CHECK_CREDENTIALS_SUCCESS
+    type: types.CHECK_CREDENTIALS_SUCCESS,
   };
 }
 export function checkCredentialsFailure() {
   return {
-    type: types.CHECK_CREDENTIALS_FAILURE
+    type: types.CHECK_CREDENTIALS_FAILURE,
   };
 }
 export function addCredentials() {
   return {
-    type: types.ADD_CREDENTIALS
+    type: types.ADD_CREDENTIALS,
   };
 }
 export function addCredentialsSucess() {
   return {
-    type: types.ADD_CREDENTIALS_SUCCESS
+    type: types.ADD_CREDENTIALS_SUCCESS,
   };
 }
+
 export function addCredentialsFailure(hint) {
   return {
     type: types.ADD_CREDENTIALS_FAILURE,
-    hint
+    hint,
+  };
+}
+
+export function addCredntialCreate(hint) {
+  return {
+    type: types.ADD_CREDENTIALS_FAILURE,
+    hint,
   };
 }
